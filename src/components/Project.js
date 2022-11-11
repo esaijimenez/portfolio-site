11e04c302453cca015777f1377356adb67432262
root <root@Esais-Rig.localdomain>
2022-11-11 17:14:37 -0600

    Add existing file

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,7 @@
 import React from "react";
-import uniswap from '../images/uniswap.png';
-import compound from '../images/compound.png';
+import project1 from '../images/comingsoon.jpg';
+import project2 from '../images/comingsoon.jpg';
+import portfolio from '../images/pdw.png';
 
 export default function Project() {
     return (
@@ -10,28 +11,36 @@ export default function Project() {
             <div className="projects__cards">
 
                 <div className="projects__card">
-                    <h3>Uniswap</h3>
-                    <img src={uniswap} alt="Uniswap Swap Page" />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Facilis consectetur facere neque assumenda quasi aliquid,
-                        perferendis ratione rerum dolorem.
+                    <h3>Coming Soon...</h3>
+                    <img src={project1} alt="Project 1 Page" />
+                    <p>This is where my first project is going to live
+                        whenever it is created.
                     </p>
 
-                    <a href="https://uniswap.org/" target="_blank" className="button">Site</a>
-
-                    <a href="https://github.com/Uniswap/v2-core" target="_blank" className="button">Code</a>
+                    <a href="https://esaijimenez.on.fleek.co/" target="_blank" className="button">Site</a>
+                    <a href="https://esaijimenez.on.fleek.co/" target="_blank" className="button">Code</a>
                 </div>
 
                 <div className="projects__card">
-                    <h3>Compound</h3>
-                    <img src={compound} alt="Compound Landing Page" />
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Facilis consectetur facere neque assumenda quasi aliquid,
-                        perferendis ratione rerum dolorem.
+                    <h3>Coming Soon...</h3>
+                    <img src={project2} alt="Project 2 Page" />
+                    <p>This is where my second project is going to live
+                        whenever it is created.
                     </p>
 
-                    <a href="https://compound.finance/" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/compound-finance/compound-protocol" target="_blank" className="button">Code</a>
+                    <a href="https://esaijimenez.on.fleek.co/" target="_blank" className="button">Site</a>
+                    <a href="https://esaijimenez.on.fleek.co/" target="_blank" className="button">Code</a>
+                </div>
+
+                <div className="projects__card">
+                    <h3>Portfolio Website</h3>
+                    <img src={portfolio} alt="Portfolio Website Page" />
+                    <p>A website I created for my portfolio to show off some of
+                        my most notable projects and introduce myself.
+                    </p>
+
+                    <a href="https://esaijimenez.on.fleek.co/" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/esaijimenez/portfolio-site" target="_blank" className="button">Code</a>
                 </div>
             </div>
         </section>
